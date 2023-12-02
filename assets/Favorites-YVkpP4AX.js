e64c1cb9ddf238a71e2920d9e4933cedb1c18088
livegp <livegp@users.noreply.github.com>
2023-12-02 18:15:22 +0000

    deploy: 06b956a1cfce426290e0a04d7f863cbb9983f8be

--- assets/Favorites-YVkpP4AX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-b12MS36t.js";function e(){return r.jsx("h2",{children:" Favorites "})}export{e as default};
--- assets/Home-oGSc3qhY.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-b12MS36t.js";function e(){return r.jsx("h1",{children:"AUTOMARKET"})}export{e as default};

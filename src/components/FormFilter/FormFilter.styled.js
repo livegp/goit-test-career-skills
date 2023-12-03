bb16b473a9a1e978c9f973bac0f0555f60465847
Oleksandr <live.gp@gmail.com>
2023-12-03 23:52:53 +0200

    Create FormFilter.styled.js

---
+++ src/components/FormFilter/FormFilter.styled.js
@@ -0,0 +1,61 @@
+import { Form, Field } from 'formik';
+import styled from 'styled-components';
+
+export const SearchForm = styled(Form)`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 300px;
+  border: 2px solid #dfe3ee;
+  background-color: transparent;
+  border-radius: 4px;
+  overflow: hidden;
+  order: 3;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    border-color: #f7f7f7;
+  }
+  /* @media (width <= 576px) {
+    display: none;
+  } */
+`;
+
+export const SearchFormLabel = styled.label`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  clip-path: inset(50%);
+  border: 0;
+`;
+
+export const SearchFormInput = styled(Field)`
+  display: inline-block;
+  width: 100%;
+  font: inherit;
+  color: #dfe3ee;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  padding-left: 4px;
+  padding-right: 4px;
+  background-color: transparent;
+  color-scheme: dark;
+
+  &::placeholder {
+    background-color: transparent;
+    color: #dfe3ee;
+    font: inherit;
+    font-size: 18px;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus {
+    outline: 3px solid black;
+  }
+`;
